46d0efc40dc9c60bbea2d6ed1bec13229956609f
Liz Haakenson <lhaakens3@gmail.com>
2016-09-08 16:14:39 -0500

    created pop up window in route map with interactive buttons

--- server/public/scripts/controllers/routeCtrl.js
+++ server/public/scripts/controllers/routeCtrl.js
@@ -1,6 +1,18 @@
 ssApp.controller("RouteCtrl", ["$scope", "$http", "$location", function($scope, $http, $location) {
   console.log("RouteCtrl works");
 
+  $scope.activate = function () {
+    console.log('activate button was clicked!');
+  }
+
+  $scope.warning = function () {
+    console.log('warning button was clicked!');
+  }
+
+  $scope.cancel = function () {
+    console.log('cancel button was clicked!');
+  }
+
 }]);
 
 
