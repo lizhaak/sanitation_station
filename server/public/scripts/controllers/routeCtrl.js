eaf1a5096b98f9924f1add04126ac607d3dabc0d
Liz Haakenson <lhaakens3@gmail.com>
2016-09-09 11:51:28 -0500

    started again with ng-map to have binding clickable events in infoWindow on map

--- server/public/scripts/controllers/routeCtrl.js
+++ server/public/scripts/controllers/routeCtrl.js
@@ -1,115 +1,44 @@
-ssApp.controller("RouteCtrl", ["$scope", "$http", "$location", function($scope, $http, $location) {
+ssApp.controller("RouteCtrl", ["$scope", "$http", "$location", "NgMap", function($scope, $http, $location, NgMap) {
   console.log("RouteCtrl works");
-  // $scope.goLocation = false;
 
-  //  $scope.vm = this;
+  $scope.vm = this;
+  NgMap.getMap().then(function(map) {
+    console.log('map', map);
+    $scope.vm.map = map;
+  });
 
+  $scope.vm.activateClicked = function() {
+    console.log('Activate click a link inside infoWindow');
+  };
 
+  $scope.vm.warningClicked = function() {
+    console.log('Warning click a link inside infoWindow');
+  };
 
-  // $scope.addressClicked = function (event) {
-  //   console.log('a location balloon was clicked!');
-  //   if($scope.goLocation === false) {
-  //     $scope.goLocation = true;
-  //     console.log('goLocation', $scope.goLocation);
-  //   }
-  // }
+  $scope.vm.cancelClicked = function() {
+    console.log('Cancel click a link inside infoWindow');
+  };
 
-  // $scope.showModal = false;
-  // $scope.buttonClicked = "";
-  // $scope.addressClicked = function(btnClicked){
-  //     $scope.buttonClicked = btnClicked;
-  //     $scope.showModal = !$scope.showModal;
-  // };
-  initMap();
+  $scope.vm.locations = [
+    {account_id: 1234, address: '1303 Eagle Bluff Drive', cityStateZip: 'Hastings, MN 55033', position:[44.75,-92.87]},
+    {account_id: 1244, address: '1541 Test Driver', cityStateZip: 'Hastings, MN 55033', position:[44.72,-92.85]}
+  ];
+  $scope.vm.location = $scope.vm.locations[0];
 
-  function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: {lat: 44.75, lng: -92.87 } // north, west
-        });
+  $scope.vm.showDetail = function(e, location) {
+    $scope.vm.location = location;
+    console.log('location: ', $scope.vm.location);
+    console.log('location0: ', location);
+    $scope.vm.map.showInfoWindow('infoWindow', this.getPosition());
+  };
 
-        var bounds = {
-          north: 44.75,
-          south: 44.72,
-          east: -92.85,
-          west: -92.87
-        };
-
-        // Display the area between the location southWest and northEast.
-        map.fitBounds(bounds);
-
-        // Add 5 markers to map at random locations.
-        // For each of these markers, give them a title with their index, and when
-        // they are clicked they should open an infowindow with text from a secret
-        // message.
-        var secretMessages = ['This', 'is', 'the', 'secret', 'message'];
-        var locationObject = {
-          address: "1303 Eagle Bluff Drive",
-          cityStateZip: "Hastings, MN 55033",
-          account_num: 1234,
-          active: true,
-          warning: false,
-          cancel: false
-        }
-
-
-        var contentString = "<custom-marker>" +
-                          "<div class='address'>" + locationObject.address + "</div>" +
-                      "<div class='address'>" + locationObject.cityStateZip + "</div>" +
-                      "<div class='accountNum'><span>Account #: </span>" + locationObject.account_num + "</div>" +
-                      "<div class='customerActive'>" +
-                        "<button onclick='activate()'>Activate</button>" +
-                      "</div>" +
-                      "<div class='customerWarn'>" +
-                        "<button onclick='warning()'>Warning</button>" +
-                      "</div>" +
-                      "<div class='customerCancel'>" +
-                        "<button onclick='cancel()'>Cancel</button>" +
-                      "</div>" +
-                    "</div>" +
-                    "</custom-marker>";
-
-        var lngSpan = [-92.87, -92.85];
-        var latSpan = [44.75, 44.72];
-        for (var i = 0; i < lngSpan.length; ++i) {
-          var marker = new google.maps.Marker({
-            position: {
-              lat: latSpan[i],
-              lng: lngSpan[i]
-            },
-            map: map,
-            icon: '/styles/darkgreen_MarkerS.png'
-          });
-          attachLocationOptions(marker, contentString);
-        }
-      }
-
-      // Attaches an info window to a marker with the provided message. When the
-      // marker is clicked, the info window will open with the secret message.
-      function attachLocationOptions(marker, contentString) {
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
-
-        marker.addListener('click', function() {
-          infowindow.open(marker.get('map'), marker);
-        });
-      }
-
-      // $scope.activate = function () {
-      //   console.log('activate button was clicked!');
-      // }
-
-      function activate() {
-        console.log('activate button was clicked!');
-      }
-
-      $scope.warning = function () {
-        console.log('warning button was clicked!');
-      }
-
-      $scope.cancel = function () {
-        console.log('cancel button was clicked!');
-      }
+  $scope.vm.hideDetail = function() {
+    $scope.vm.map.hideInfoWindow('infoWindow');
+  };
 
 }]);
+
+
+// ssApp.run(function() {
+//   console.log("ssApp is ready!");
+// });
